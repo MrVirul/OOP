40271c7f3f73d5eb776bed7fda02b421c5282e3a
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-04-12 01:23:54 +0530

    Fix language level in project configuration and add SavingAccount class with default constructor and getter/setter methods

---
+++ src/TUTE01/SavingAccount.java
@@ -0,0 +1,24 @@
+package TUTE01;
+
+public class SavingAccount {
+    private int accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    //default constructor
+    public SavingAccount(){
+        this.accountNumber = 0;
+        this.accountHolderName = "No name";
+        this.balance = 0.00;
+    }
+    public String getaccountHolderName(){
+        return accountHolderName;
+    }
+    public void setAccountHolderName(String accountHolderName){
+        this.accountHolderName = accountHolderName;
+    }
+public static void main(String[] args) {
+    
+}
+
+}
