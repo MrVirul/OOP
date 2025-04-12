f736341ea6b1f288d98dc7a724876495c8644de6
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-04-12 21:26:21 +0530

    Fix OTRate type and update employee type handling in EmployeeSalary class

--- src/TUTE0102/EmployeeSalary.java
+++ src/TUTE0102/EmployeeSalary.java
@@ -4,13 +4,15 @@ public class EmployeeSalary {
     private char employeeType;
     private double BasicSalary;
     private int OTHourse;
-    private double OTRate;
+    private int OTRate;
+    private double totalSalary;
 
     //default constructor
     public EmployeeSalary() {
         this.employeeType = '0';
         this.BasicSalary = 0.0;
         this.OTHourse = 0;
+        this.OTRate = 0;
     }
 
     //overloaded constructor
@@ -20,15 +22,27 @@ public class EmployeeSalary {
         this.OTHourse = OTHourse;
     }
 
-    public char getEmployeeType(double OTRate){
-        if(employeeType == '1'){
-            return OTRate = 1000;
-        }else if(employeeType == '2'){
-            return OTRate = 1500;
-        }else if(employeeType == '3'){
-            return OTRate = 1700;
-    }else{
-            return OTRate = 0;
-        }
+    public void setEmployeeType(char employeeType) {
+        this.employeeType = employeeType;
     }
+
+    if(employeeType == '1'){
+        this.OTRate = 1000;
+    }else if(employeeType == '2'){
+        this.OTRate = 1500;
+    }else if(employeeType == '3'){
+        this.OTRate = 1700;
+    }else {
+        System.out.println("Invalid employee type");
+        this.OTRate = 0;
+    }
+
+    public set BasicSalary(double BasicSalary) {
+        this.BasicSalary = BasicSalary;
+    }
+    public void setOTHourse(int OTHourse) {
+        this.OTHourse = OTHourse;
+    }
+
+    
 }
