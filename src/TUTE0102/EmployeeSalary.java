654e868f7aa6383e33b39617cf6dff876c1c393a
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-04-12 21:09:59 +0530

    Add default constructor to EmployeeSalary class for initialization

--- src/TUTE0102/EmployeeSalary.java
+++ src/TUTE0102/EmployeeSalary.java
@@ -5,6 +5,13 @@ public class EmployeeSalary {
     private double BasicSalary;
     private int OTHourse;
 
+    //default constructor
+    public EmployeeSalary() {
+        this.employeeType = '0';
+        this.BasicSalary = 0.0;
+        this.OTHourse = 0;
+    }
 
+   
 
 }
