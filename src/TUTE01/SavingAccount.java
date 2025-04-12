6e37713576178db3cb089cfbb2cd894e5ca3e32d
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-04-12 14:41:44 +0530

    Update project language level to JDK_24 and improve withdrawAmount method in SavingAccount class

--- src/TUTE01/SavingAccount.java
+++ src/TUTE01/SavingAccount.java
@@ -1,5 +1,6 @@
 package TUTE01;
 
+import java.util.Scanner;
 public class SavingAccount {
     private int accountNumber;
     private String accountHolderName;
@@ -35,13 +36,13 @@ public class SavingAccount {
             System.out.println("Deposited " + amount + " to account number " + accountNumber + " account balance is " + balance);
         }else{
             System.out.println("Invalid amount to deposit!!!");
-        }
+        }}
         public void withdrawAmount(double amount){
             if(amount > 0 && amount <= balance){
                 this.balance = this.balance - amount;
-                System.out.println("Withdrawn " + amount + " from account number " + accountNumber + " account balance is " + balance);
+                System.out.println("Withdraw " + amount + " from account number " + accountNumber + " account balance is " + balance);
             }
-        }else{
+        else{
             System.out.println("Invalid amount to withdraw!!!");
         }
     }
@@ -51,7 +52,21 @@ public class SavingAccount {
         System.out.println("Account Balance: " + balance);
     }
 public static void main(String[] args) {
-    
-}
+    Scanner input = new Scanner(System.in);
+    SavingAccount account1 = new SavingAccount();
+
+    System.out.print("Enter account number: ");
+    account1.setAccountNumber(input.nextInt());
+
+    System.out.print("Enter account holder name: ");
+    account1.setAccountHolderName(input.nextLine());
+
+    System.out.print("Enter initial deposit: ");
+
+
+
+
+
+    }
 
 }
