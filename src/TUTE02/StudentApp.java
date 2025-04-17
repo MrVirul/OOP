68436a49371ed64ca5938bdf3e68a002ca01f019
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-04-17 17:56:59 +0530

    Improve user prompts in StudentApp for better clarity

--- src/TUTE02/StudentApp.java
+++ src/TUTE02/StudentApp.java
@@ -8,8 +8,15 @@ public class StudentApp {
         Student student1 = new Student();
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter Student ID:");
+        System.out.println("Enter Student ID: ");
         student1.setStudentID(input.nextInt());
 
+        System.out.println("Enter Student Name: ");
+        student1.setStudentName(input.nextLine());
+
+        System.out.println("Enter Student GPA: ");
+        student1.setGPA(input.nextDouble());
+
+        student1.displayStudentDetails();
     }
 }
