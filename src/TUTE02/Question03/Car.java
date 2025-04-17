adedcdf3ec5c5bd357fc2b0ab6fb0d6fc8670d51
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-04-17 19:29:01 +0530

    Add properties and default constructor to Car class

--- src/TUTE02/Question03/Car.java
+++ src/TUTE02/Question03/Car.java
@@ -1,4 +1,15 @@
 package TUTE02.Question03;
 
 public class Car {
+    private String registrationNumber;
+    private String modelName;
+    private double price;
+
+    public Car(){
+        this.registrationNumber = "Not Registered";
+        this.modelName = "Not Specified";
+        this.price = 0.0;
+    }
+
+   
 }
