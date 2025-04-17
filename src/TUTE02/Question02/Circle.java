8036873c48e05ce4581d3b7fb24c21180c4d04f5
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-04-17 18:22:09 +0530

    Refactor Circle class: reorganize constructors and methods for improved readability

--- src/TUTE02/Question02/Circle.java
+++ src/TUTE02/Question02/Circle.java
@@ -4,14 +4,22 @@ public class Circle {
 
     private double radius;
 
+    //default constructor
     public  Circle(){
         this.radius = 0.0;
     }
     
+    //constructor with parameter
     public Circle(double radius) {
         this.radius = radius;
     }
 
-    
+    public double calculateArea01(){
+        return Math.PI * radius * radius;
+    }
 
+    public double calculateArea02(){
+        double radiusFromDiameter = radius / 2;
+        return Math.PI * radiusFromDiameter * radiusFromDiameter;
+    }
 }
