c7108a72a317ccb1681c621abfddcec5b5fb2185
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-04-17 19:37:22 +0530

    Add main method to CarApp for creating and initializing Car instances

--- src/TUTE02/Question03/CarApp.java
+++ src/TUTE02/Question03/CarApp.java
@@ -1,4 +1,13 @@
 package TUTE02.Question03;
 
 public class CarApp {
+    public static void main(String []args){
+
+        Car car1 = new car();
+
+        Car car2 = new Car();
+        car2.setRegistrationNumber("CAB - 8209");
+        car2.setModelName("Nissan Leaf");
+        car2.setPrice(3500000.0);
+    }
 }
