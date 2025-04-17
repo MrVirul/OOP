8e8a4dcc53bedc43e1fed12ec013eeafa30b6331
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-04-17 23:55:58 +0530

    Add getter and setter methods for Customer class fields

--- src/TUTE02/Question04/Customer.java
+++ src/TUTE02/Question04/Customer.java
@@ -16,6 +16,30 @@ public class Customer {
         this.customerName = customerName;
         this.unitConsumed = unitConsumed;
     }
+    public int getCustomerId() {
+        return customerId;
+    }
 
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getUnitConsumed() {
+        return unitConsumed;
+    }
+
+    public void setUnitConsumed(int unitConsumed) {
+        this.unitConsumed = unitConsumed;
+    }
+
+  
 
 }
