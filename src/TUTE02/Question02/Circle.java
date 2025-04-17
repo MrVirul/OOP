f67f2cb901681218b3591538d2c20fa9d87ba24f
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-04-17 19:23:16 +0530

    Implement CircleApp for user input and area calculation; update Circle class methods

--- src/TUTE02/Question02/Circle.java
+++ src/TUTE02/Question02/Circle.java
@@ -3,20 +3,13 @@ package TUTE02.Question02;
 import java.util.Scanner;
 
 public class Circle {
-
     private double radius;
 
-    //default constructor
-    public  Circle(){
+    public Circle() {
         this.radius = 0.0;
     }
-    
-    //constructor with parameter
-    public Circle(double radius) {
-        this.radius = radius;
-    }
 
-    public double getRadius(){
+    public double getRadius() {
         return radius;
     }
 
@@ -24,24 +17,14 @@ public class Circle {
         this.radius = radius;
     }
 
-    public double calculateArea01(){
-        return Math.PI * radius * radius;
+    public double calculateArea() {
+        return (22 / 7.0) * radius * radius;
     }
 
-    public double calculateArea02(){
-        double radiusFromDiameter = radius / 2;
-        return Math.PI * radiusFromDiameter * radiusFromDiameter;
-    }
-
-    public static void main(String[] args) {
-        
-        Scanner input = new Scanner(System.in);
-
-        Circle circle1 = new Circle();
-
-        System.out.print("Enter the radius of the circle: ");
-        input.circle1.setRadius(input.nextDouble());
-        circle1.calculateArea01();
+    public double calculateArea(double diameter) {
+        double radius = diameter / 2;
+        return (22 / 7.0) * radius * radius;
     }
 }
+
 
--- src/TUTE02/Question02/CircleApp.java
+++ src/TUTE02/Question02/CircleApp.java
@@ -1,5 +1,26 @@
 package TUTE02.Question02;
 
-public class CircleApp {
+import java.util.Scanner;
 
+public class CircleApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Circle circle = new Circle();
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+        circle.setRadius(radius);
+
+        double areaUsingRadius = circle.calculateArea();
+        System.out.println("Area of the circle using radius: " + areaUsingRadius);
+
+        System.out.print("Enter the diameter of the circle: ");
+        double diameter = scanner.nextDouble();
+
+        double areaUsingDiameter = circle.calculateArea(diameter);
+        System.out.println("Area of the circle using diameter: " + areaUsingDiameter);
+
+        scanner.close();
+    }
 }
+
