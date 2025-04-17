e9c5b2c112dafd3cd6ec754038249a1bc25ee1a7
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-04-17 18:16:26 +0530

    Add Circle class with constructors for radius initialization

---
+++ src/TUTE02/Question02/Circle.java
@@ -0,0 +1,17 @@
+package TUTE02.Question02;
+
+public class Circle {
+
+    private double radius;
+
+    public  Circle(){
+        this.radius = 0.0;
+    }
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    
+
+}
