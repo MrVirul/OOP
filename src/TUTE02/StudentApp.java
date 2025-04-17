bfc1ddacd26d5311bf8fc75fdba6b95f4d7e1ec5
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-04-17 16:00:46 +0530

    Add user input for Student ID in StudentApp class

--- src/TUTE02/StudentApp.java
+++ src/TUTE02/StudentApp.java
@@ -1,10 +1,15 @@
 package TUTE02;
 
+import java.util.Scanner;
+
 public class StudentApp {
     public static void main(String[] args) {
-        
-        Student student1 = new Student();
 
+        Student student1 = new Student();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Student ID:");
+        student1.setStudentID(input.nextInt());
 
     }
 }
