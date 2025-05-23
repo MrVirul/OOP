0755dfd6f917294492d09a66f7bf2ecd4dd42528
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-05-24 00:53:59 +0530

    Remove Processor, Smartphone, Car, Engine, and Vehicle classes along with their associated application classes

--- src/TUTE04/Question01/SmartApp.java
+++
@@ -1,8 +0,0 @@
-package TUTE04.Question01;
-
-public class SmartApp {
-    public static void main(String[] args) {
-        Smartphone smartphone01 = new Smartphone(5000, "Snapdragon 888");
-        smartphone01.showSmartPhoneDetails();
-    }
-}
--- src/TUTE04/Question02/CarApp.java
+++
@@ -1,8 +0,0 @@
-package TUTE04.Question02;
-
-public class CarApp {
-    public static void main(String[] args) {
-        Car car = new Car("Toyota", 120, "Corolla", "V6");
-        car.showVehicleDetails();
-    }
-}
