5033d8b31685dd98c3ae3408a961da7ac55c925f
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-06-02 01:20:47 +0530

    Add Book class implementation to Lab08.Question02 package

---
+++ src/Lab08/Question02/Book.java
@@ -0,0 +1,9 @@
+package Lab08.Question02;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private Library library;
+
+
+}
---
+++ src/Lab08/Question02/Library.java
@@ -0,0 +1,4 @@
+package Lab08.Question02;
+
+public class Library {
+}
