e2b757e78ef09315fbbb805bd427941553232a2b
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-06-02 22:25:20 +0530

    Add FinalModelPaper package with LibraryBook and LibraryApp implementations

---
+++ src/FinalModelPaper/LibraryApp.java
@@ -0,0 +1,51 @@
+package FinalModelPaper;
+
+import java.util.Scanner;
+
+public class LibraryApp {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter book 1 details ");
+        System.out.print("Enter book title : ");
+        String bookTitle1 = input.nextLine();
+        System.out.print("Enter book ID : ");
+        String bookId1 = input.nextLine();
+        System.out.print("Enter book author name : ");
+        String author1 = input.nextLine();
+        System.out.print("Enter numbers of copies available : ");
+        int copiesAvailable1 = input.nextInt();
+        input.nextLine();
+        System.out.println();
+
+        LibraryBook book1 = new LibraryBook(bookTitle1,bookId1,author1,copiesAvailable1);
+
+        System.out.println("Enter book 2 details ");
+        System.out.print("Enter book 2 title : ");
+        String bookTitle2 = input.nextLine();
+        System.out.print("Enter book ID : ");
+        String bookId2 = input.nextLine();
+        System.out.print("Enter book author name : ");
+        String author2 = input.nextLine();
+        System.out.print("Enter numbers of copies available : ");
+        int copiesAvailable2 = input.nextInt();
+        input.nextLine();
+        System.out.println();
+
+        LibraryBook book2 = new LibraryBook(bookTitle1,bookId1,author1,copiesAvailable1);
+
+        System.out.print("Enter the copies you want for book 1 :");
+        int requestedCopies1 = input.nextInt();
+        book1.lendBook(requestedCopies1);
+
+        System.out.print("Enter the copies you want for book 2 :");
+        int requestedCopies2 = input.nextInt();
+        book2.lendBook(requestedCopies2);
+
+        System.out.println("Book details after issuing");
+        System.out.println("--------------------------");
+        book1.printBookDetails();
+        book2.printBookDetails();
+    }
+}
---
+++ src/FinalModelPaper/LibraryBook.java
@@ -0,0 +1,41 @@
+package FinalModelPaper;
+
+public class LibraryBook {
+
+    private String bookId;
+    private String title;
+    private String author;
+    private int copiesAvailable;
+
+    public LibraryBook() {
+        this.bookId = "0000";
+        this.title = "No title";
+        this.author = "No author";
+        this.copiesAvailable = 0;
+    }
+
+    public LibraryBook(String title, String bookId, String author, int copiesAvailable) {
+        this.title = title;
+        this.bookId = bookId;
+        this.author = author;
+        this.copiesAvailable = copiesAvailable;
+    }
+
+    void lendBook(int requestedCopies){
+        if(requestedCopies > copiesAvailable){
+            System.out.println("Not enough copies available. Please check back later.");
+        } else {
+            copiesAvailable = copiesAvailable - requestedCopies;
+            System.out.println("Book issued successfully");
+        }
+    }
+
+    void printBookDetails(){
+        System.out.println("====Datils of books===");
+        System.out.println("Book title is : ");
+        System.out.println(title + " 's book ID is : " + bookId);
+        System.out.println(title + " 's author is : " + author);
+        System.out.println("Available copies : " + copiesAvailable);
+        System.out.println();
+    }
+}
