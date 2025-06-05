2dbf310dbc4071cc008acbde8eae23a9b0cc69e8
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-06-05 10:42:56 +0530

    Add Kuppi package with Animal, Cat, Dog, and AnimalApp class implementations

---
+++ src/Kuppi/Animal.java
@@ -0,0 +1,9 @@
+package Kuppi;
+
+public abstract class Animal {
+
+    public abstract String  eat();
+
+    public abstract String makeSound();
+}
+
---
+++ src/Kuppi/Cat.java
@@ -0,0 +1,16 @@
+package Kuppi;
+
+public class Cat extends Animal{
+
+
+
+    @Override
+    public String makeSound(){
+        return "Cat meaw";
+    }
+
+    @Override
+    public String eat(){
+        return "Cat is eating";
+    }
+}
