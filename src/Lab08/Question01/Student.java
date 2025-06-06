684a7105b7f6a9692b7a00ad7d0b8bfc08918ec0
Virul Methdinu Meemana <virul.mmeemana@gmail.com>
2025-06-06 14:56:08 +0530

    Re-add Student class to Lab08.Question01 package with constructors, methods to enroll courses, and student details display functionality.

--- src/Lab08/Question01/Student.java
+++ src/Lab08/Question01/Student.java
@@ -1,4 +1,41 @@
 package Lab08.Question01;
 
 public class Student {
+    private String studentID;
+    private String studentName;
+    private int courseCount;
+    Course[] course = new Course[3];
+
+    public Student(String studentID, String studentName, int courseCount) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.courseCount = courseCount;
+    }
+
+    public Student (){
+        this.studentName = "Null";
+        this.studentID = "Null";
+        this.courseCount = 0;
+        Course course1 = new Course("IT00","Null");
+    }
+    public void enrollCourse(Course C){
+        if (courseCount < course.length) {
+            course[courseCount] = C;
+            courseCount++;
+        } else {
+            System.out.println("Cannot enroll in more courses. Maximum limit reached.");
+        }
+    }
+
+    public void displayStudentDetails() {
+        System.out.println("Student ID  is : " + studentID);
+        System.out.println("Student Name is : " + studentName);
+        System.out.println("Student course count is : " + courseCount);
+
+        for (int i = 0; i >= courseCount; i++){
+             if (course[i] != null) {
+                 System.out.println("Course " + (i+1) + ": " + course[i].courseCode + " - " + course[i].courseTitle);
+            }
+        }
+    }
 }
